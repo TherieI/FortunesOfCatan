1971430e89485e924198e2cd8c0625197815a4f9
TherielGabriel <rosasalgabriel@gmail.com>
2024-03-20 11:35:06 -0700

    added assets

--- src/settlers/interface/ui.rs
+++ src/settlers/interface/ui.rs
@@ -1,4 +1,15 @@
+use glium::Frame;
+use glium::backend::Facade;
 
-pub struct Interface {
-    
+/// A panel that is drawn to the screen
+pub trait Interface {
+    /// Draw the interface
+    fn draw<F>(&self, facade: &F, frame: Frame) -> Frame
+    where
+        F: ?Sized + Facade;
+}
+
+/// Draw information about the user's hand
+pub struct Hand {
+
 }
